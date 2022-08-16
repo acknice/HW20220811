67030f395fc3b5e0e34c69202d734b2475df56f0
acknice <acknicely@gmail.com>
2022-08-15 23:26:53 -0700

    initialized branch

---
+++ src/PlayingAroundWithDataTypes.java
@@ -0,0 +1,30 @@
+
+public class PlayingAroundWithDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int one = 1;
+		int two = 2;
+		one -= two;
+		
+		System.out.println(one);
+		
+		//int tooBig = 3677849006388574902776546599450038266562;
+		
+		//System.out.println(tooBig);
+		
+		float tooSmall = 0.154616846316846161651684616184613154816846168764131314687613138741651687461313546513134313138131846413134133843131863131834313135466118f;
+		
+		System.out.println(tooSmall);
+		
+		String testString = "This is a test";
+
+		System.out.println(testString);
+		
+		Integer blank;
+		
+		//System.out.println(blank);
+	}
+
+}
